43f9378e22dd7dc0cff588265eef712b72ea6b53
zhangxianjun <xianjun.zhang@starwin.com>
2019-04-16 11:24:59 +0800

    update upload_to_bintray.sh

--- buildSrc/upload_to_bintray.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# @author Summer Heart
-#
-
-../gradlew clean build
-../gradlew generatePomFileForMavenPublication
-../gradlew publishMavenPublicationToMavenLocal
-../gradlew bintrayUpload -PbintrayUser=$* -PbintrayKey=$* -PdryRun=false
